2ada74733820cbb4ff477c00e2316ab98a02ae81
Jiyeon-Kang <kang158632@gmail.com>
2024-03-25 23:06:48 +0900

    feat: add header, footer

--- my-portfolio/componenets/layout.js
+++ my-portfolio/componenets/layout.js
@@ -1,8 +1,12 @@
+import Header from "@/componenets/header";
+import Footer from "@/componenets/footer";
 export default function Layout({children}) {
     return (
         <>
+            <Header/>
             <h1>Layout</h1>
             <div>{children}</div>
+            <Footer/>
         </>
     );
 }
