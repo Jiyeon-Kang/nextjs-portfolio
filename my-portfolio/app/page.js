877501c4b9ca11f661030338e07e76970fc7d95d
Jiyeon-Kang <kang158632@gmail.com>
2024-04-15 22:05:32 +0900

    feat: add header

--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Header from "@/app/components/hesder";
 
 export default function Home() {
     return (
-        <div>
-            <h1>Jiyeon's Portfolio</h1>
-        </div>
+        <>
+            <Header/>
+            <h1 className="text-3xl font-bold underline">
+                Home
+            </h1>
+        </>
     );
 }
