85aab11fcfdcbf42bc604c15293bc9f5f8c00e36
Jiyeon-Kang <kang158632@gmail.com>
2024-04-22 23:47:06 +0900

    feat: modify layout

--- my-portfolio/app/components/footer.js
+++ my-portfolio/app/components/footer.js
@@ -4,14 +4,9 @@ export default function Footer() {
             <div className="bg-gray-100">
                 <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
                     <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
-                             stroke-linejoin="round" stroke-width="2"
-                             className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
-                        <span className="ml-3 text-xl">Tailblocks</span>
+                        <span className="ml-3 text-xl">Jiyeon's Portfolio</span>
                     </a>
-                    <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
+                    <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2024 Jiyeon's Portfolio —
                         <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1"
                            target="_blank">@knyttneve</a>
                     </p>
--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -1,14 +1,17 @@
 import Image from "next/image";
-import Header from "@/app/components/hesder";
+import Header from "@/app/components/header";
 import Footer from "@/app/components/footer";
+import Hero from "@/app/components/home/hero";
 
 export default function Home() {
     return (
         <>
             <Header/>
-            <h1 className="text-3xl font-bold underline">
-                Home
-            </h1>
+            <section className=" flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+                <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                    <Hero/>
+                </div>
+            </section>
             <Footer/>
         </>
     );
