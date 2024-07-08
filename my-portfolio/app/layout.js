9dd7e315b8e46bf2ae3a82b7b6ec570f8bec56f4
Jiyeon-Kang <kang158632@gmail.com>
2024-07-08 11:34:24 +0900

    fix: fix browser warning

--- my-portfolio/app/layout.js
+++ my-portfolio/app/layout.js
@@ -1,5 +1,5 @@
 import {Inter} from "next/font/google";
-import { ThemeProvider } from 'next-themes'
+import {ThemeProvider} from 'next-themes'
 import "./globals.css";
 
 export const metadata = {
@@ -13,7 +13,7 @@ const inter = Inter({subsets: ["latin"]});
 export default function RootLayout({children}) {
     return (
         <html suppressHydrationWarning lang="en">
-        <body className={inter.className}>
+        <body className={inter.className} suppressHydrationWarning={true}>
         <ThemeProvider>{children}</ThemeProvider>
         </body>
         </html>
