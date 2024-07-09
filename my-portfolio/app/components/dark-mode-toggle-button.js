ac62aef61a60f857b6cb36e397b526029a85fb19
Jiyeon <kang158632@gmail.com>
2024-07-09 14:21:02 +0900

    fix: fix error between SSR and SCR of dark-mode

--- my-portfolio/app/components/dark-mode-toggle-button.js
+++ my-portfolio/app/components/dark-mode-toggle-button.js
@@ -1,9 +1,20 @@
 'use client';
 
 import {useTheme} from 'next-themes'
+import { useEffect, useState } from 'react';
+
 
 export default function DarkModeToggleButton() {
     const {theme, setTheme} = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
 
     return (<>
         <button
