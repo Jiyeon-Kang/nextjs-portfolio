4fff22ef27f43e849df112d860d29c48acc74bc9
Jiyeon <kang158632@gmail.com>
2024-08-27 16:34:51 +0900

    style: delete comment

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -2,26 +2,22 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from 'next-themes';
 import "../app/styles/globals.css";
 
-// Metadata 인터페이스 정의
 interface Metadata {
     title: string;
     description: string;
     content: string;
 }
 
-// 실제 메타데이터 객체는 타입을 별도로 정의하지 않습니다.
 export const metadata: Metadata = {
     title: "Jiyeon's Portfolio",
     description: "Jiyeon's React-Portfolio",
     content: "Jiyeon's Portfolio",
 };
 
-// Inter 폰트 설정
 const inter = Inter({ subsets: ["latin"] });
 
-// RootLayout 컴포넌트의 타입 정의
 interface RootLayoutProps {
-    children: React.ReactNode; // children의 타입을 React.ReactNode로 설정
+    children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
