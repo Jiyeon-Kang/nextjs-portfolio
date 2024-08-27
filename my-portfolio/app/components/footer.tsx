f5e2bdee7f8142065afeb697556431a73ff26531
Jiyeon <kang158632@gmail.com>
2024-08-27 16:32:08 +0900

    feature: add GitHub icon

--- my-portfolio/app/components/footer.tsx
+++ my-portfolio/app/components/footer.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 
-export default function Footer() {
+export default function Footer(props: SVGProps<SVGSVGElement>) {
     return (
         <footer className="text-gray-600 body-font">
             <div className="bg-gray-100 dark:bg-slate-800">
@@ -14,12 +14,14 @@ export default function Footer() {
                     <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2024 Jiyeon's Portfolio
                     </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-                        <a className="ml-3 text-gray-500">
-                          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                               strokeWidth="2"
-                               className="w-5 h-5" viewBox="0 0 24 24">
-                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                        <a href="https://github.com/Jiyeon-Kang" target="_blank" className="ml-3 text-gray-500">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px"
+                               viewBox="0 0 24 24" {...props}>
+                              <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5">
+                              <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10"></path>
+                                  <path d="M14.333 19v-1.863c.025-.31-.018-.62-.126-.913a2.2 2.2 0 0 0-.5-.781c2.093-.227 4.293-1 4.293-4.544a3.48 3.48 0 0 0-1-2.434a3.2 3.2 0 0 0-.06-2.448s-.787-.227-2.607.961a9.15 9.15 0 0 0-4.666 0c-1.82-1.188-2.607-.96-2.607-.96A3.2 3.2 0 0 0 7 8.464a3.48 3.48 0 0 0-1 2.453c0 3.519 2.2 4.291 4.293 4.544a2.2 2.2 0 0 0-.496.773a2.1 2.1 0 0 0-.13.902V19"></path>
+                                  <path d="M9.667 17.702c-2 .631-3.667 0-4.667-1.948"></path>
+                              </g>
                           </svg>
                         </a>
                   </span>
