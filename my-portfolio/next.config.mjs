0f029b7593e91ee58346ec9d185cf3c1e363b26f
Jiyeon <kang158632@gmail.com>
2024-10-10 13:04:23 +0900

    add host setting

--- my-portfolio/next.config.mjs
+++ my-portfolio/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'media.gcflearnfree.org',
+            'upload.wikimedia.org'
+        ],
+    },
+};
 
 export default nextConfig;
